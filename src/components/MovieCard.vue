fdd9fff1d06c1d1d10c46c6ecdb871215b6e3540
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2022-12-24 22:50:11 +0300

    creating movie card and displaying on home page

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="rounded-lg w-40 cursor-pointer">
+        <img class="rounded-sm" :src="getImage(movie.poster_path)"/>
+    </div>
+</template>
+
+<script setup>
+import { getImage } from '@/data/MovieDb.js'
+defineProps({
+    movie:{
+        type:Object,
+        required : true
+    }
+})
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,33 +1,41 @@
 <template>
-    <div class="min-h-screen flex justify-center items-center">
-      <div class="grid gap-6">
-        <p class="text-3xl text-white text-center">
-          Bienvenue sur 
-          <span class="text-indigo-200 text-4xl font-mono 
-          uppercase font-black tracking-widest">
-            Moovizz
-          </span>
-        </p>  
-        <p class="text-xl text-white">
-          La plateforme <span class="text-indigo-300">streaming</span> du moment ! Trouvez vos films preférées
-        </p>
+    <div>
+      <div class="min-h-screen flex justify-center items-center">
+        <div class="grid gap-6">
+          <p class="text-3xl text-white text-center">
+            Bienvenue sur 
+            <span class="text-indigo-200 text-4xl font-mono 
+            uppercase font-black tracking-widest">
+              Moovizz
+            </span>
+          </p>  
+          <p class="text-xl text-white">
+            La plateforme <span class="text-indigo-300">streaming</span> du moment ! Trouvez vos films preférées
+          </p>
+        </div>
       </div>
-      <div v-for="movie in movieList" :key="movie">
-          <p>{{movie}}</p>
+      <div class="flex flex-wrap justify-center p-auto gap-2">
+        <div v-for="movie in movieList" :key="movie">
+          <MovieCard :movie="movie"></MovieCard>
+        </div>
       </div>
+     
     </div>
+    
 </template>
 
 <script setup>
-import { getPopularMovie } from '@/data/MovieDb.js'
-import ref from 'vue'
+import { getPopularMovie,getImage } from '@/data/MovieDb.js'
+import { ref } from 'vue';
+import MovieCard from '../components/MovieCard.vue';
 
 const movieList = ref();
 
 getPopularMovie().then((res) => {
   movieList.value = res.data.results
+  console.log('movieList',movieList.value)
 })
-console.log('movieList',movieList)
+
 
 </script>
 
