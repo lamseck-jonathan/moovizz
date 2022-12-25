7f7a36a1bc476e17c35735d958505ca7f8c42937
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2022-12-25 16:11:52 +0300

    movie card component

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,15 +1,53 @@
 <template>
-    <div class="rounded-lg w-40 cursor-pointer">
-        <img class="rounded-sm" :src="getImage(movie.poster_path)"/>
+    <div 
+    class="rounded-lg w-40 "
+    :class="
+    'movie-card-'+movieId,
+    props.animated ? 'cursor-pointer' : ''
+    "
+    @mouseenter="mouseEnter"
+    @mouseleave="mouseLeave"
+    >
+        <img class="rounded-lg shadow-xl" :src="getImage(props.movie.poster_path)"/>
     </div>
 </template>
 
 <script setup>
 import { getImage } from '@/data/MovieDb.js'
-defineProps({
+import {gsap} from 'gsap'
+
+const props = defineProps({
     movie:{
         type:Object,
         required : true
+    },
+    movieId:{
+        type:String,
+        required:true
+    },
+    animated:{
+        type:Boolean,
+        default:false
     }
 })
+
+function mouseEnter(){
+    if(props.animated === true)
+    gsap.fromTo('.movie-card-'+props.movieId,{
+        scale:1,
+    },
+    {
+        scale:1.2,
+    })
+}
+
+function mouseLeave(){
+    if(props.animated === true)
+    gsap.fromTo('.movie-card-'+props.movieId,{
+        scale:1.2,
+    },
+    {
+        scale:1,
+    })
+}
 </script>
