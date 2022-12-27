e19e92d89daa87f53b21dc33d295fdd207b65937
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2022-12-27 18:03:14 +0300

    layout config in router

--- src/router/index.js
+++ src/router/index.js
@@ -5,4 +5,5 @@ export const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
     routes, // short for `routes: routes`
+    linkActiveClass: 'text-white underline underline-offset-8'
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,27 @@
-import HomePage from '../pages/HomePage.vue'
+import HomePage from '@/pages/HomePage.vue'
+import DetailPage from '@/pages/DetailPage.vue'
+import MainLayout from '@/layout/MainLayout.vue'
+import SecondLayout from '@/layout/SecondLayout.vue'
 
 export const routes = [
     {
         path:'/',
-        component: HomePage,
+        component: MainLayout,
+        children:[
+            {
+                path:'/',
+                component:HomePage
+            }
+        ]
+    },
+    {
+        path:'/detail',
+        component: SecondLayout,
+        children:[
+            {
+                path:'/',
+                component:DetailPage
+            }
+        ]
     }
 ]
