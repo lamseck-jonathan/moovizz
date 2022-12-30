23607309a709f113871c0f70aa00c7f432d51d02
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2022-12-30 22:59:49 +0300

    adding function getDetailsMovie

--- src/data/MovieDb.js
+++ src/data/MovieDb.js
@@ -9,6 +9,19 @@ export async function getPopularMovie(){
     return result;
 }
 
+export async function getTopRatedMovie(){
+    const result = await axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key='
+    +apiKey+
+    '&language=en-US&page=1')
+    return result;
+}
+
+export async function getDetailsMovie(movie_id){
+    const result = await axios.get('https://api.themoviedb.org/3/movie/'
+    +movie_id+'?api_key='+apiKey+'&language=en-US')
+    return result;
+}
+
 export function getImage(path){
     return 'https://image.tmdb.org/t/p/original/'+path
 }
