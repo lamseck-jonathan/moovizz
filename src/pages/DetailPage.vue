3c3784c8bcfb7161d195523d067ae414a96ed418
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2023-01-03 17:40:18 +0300

    adding transition and loading page

--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -1,103 +1,117 @@
 <template>
-    <div class="min-h-screen flex justify-center items-center overflow-hidden bg">
-        <div class="absolute opacity-20 bg-netflix">
-            <img :src="getImage(movie.backdrop_path)"/>
-        </div>
-        <div class="relative flex flex-col ">
-            <div class="flex flex-row px-10 gap-8">
-                <img 
-                    class="rounded-lg"
-                    style="width:300px; height:400 px" 
-                    :src="getImage(movie.poster_path)" 
-                />
-                <div class="text-white pt-10">
-                    <p class="text-white font-semibold text-3xl">
-                        {{ movie.title }}
-                    </p>
-                    <p  class="flex flex-row gap-8 pt-4">
-                        <span>
-                            {{ movie.release_date}}
-                        </span> 
-                        <span v-for="genre in movie.genres" :key="genre">
-                            <p>{{genre.name}}</p>
-                        </span>
-                    </p>
-                    <p class=" pt-4 text-xl text-gray-300 italic">{{ movie.tagline }}</p>
-                    <p class="text-2xl font-semibold pt-4">Overview</p>
-                    <p class="text-white pt-4">{{ movie.overview}}</p>
-                    <div class="text-white pt-8 flex flex-row gap-8">
-                        <div>
-                            <ButtonIcon
-                            label="Notez"
-                            >
-                                <font-awesome-icon 
-                                class="text-3xl" 
-                                icon="fa-solid fa-star" 
-                                /> 
-                            </ButtonIcon>
-                        </div>
-                        <div>
-                            <ButtonIcon
-                            label="Ma liste"
-                            >
-                                <font-awesome-icon 
-                                class="text-3xl" 
-                                icon="fa-solid fa-play" 
-                                /> 
-                            </ButtonIcon>
-                        </div>
-                        <div>
-                            <ButtonIcon
-                            label="Telecharger"
-                            >
-                                <font-awesome-icon class="text-3xl"
-                                 icon="fa-solid fa-download" 
-                                 /> 
-                            </ButtonIcon>
+    <TransitionFade>
+        <LoadingPage key="1" v-if="loading"></LoadingPage>
+        <div key="2" v-else>
+            <div class="min-h-screen flex justify-center items-center overflow-hidden bg">
+                <div class="absolute opacity-50 bg-netflix">
+                    <img :src="getImage(movie.backdrop_path)"/>
+                </div>
+                <div class="relative flex flex-col">
+                    <div class="flex flex-row px-10 gap-8">
+                        <img 
+                            class="rounded-lg"
+                            style="width:300px; height:400 px" 
+                            :src="getImage(movie.poster_path)" 
+                        />
+                        <div class="text-white pt-10">
+                            <p class="text-white font-semibold text-3xl">
+                                {{ movie.title }}
+                            </p>
+                            <p  class="flex flex-row gap-8 pt-4">
+                                <span>
+                                    {{ formatDate(movie.release_date)}}
+                                </span> 
+                                <span v-for="genre in movie.genres" :key="genre">
+                                    <p>{{genre.name}}</p>
+                                </span>
+                            </p>
+                            <p class=" pt-4 text-xl text-gray-300 italic">{{ movie.tagline }}</p>
+                            <p class="text-2xl font-semibold pt-4">Description :</p>
+                            <p class="text-white pt-4 md:pr-16 sm:pr-4">{{ movie.overview}}</p>
+                            <div class="text-white pt-8 flex flex-row gap-8">
+                                <div>
+                                    <ButtonIcon
+                                    label="Notez"
+                                    >
+                                        <font-awesome-icon 
+                                        class="text-3xl" 
+                                        icon="fa-solid fa-star" 
+                                        /> 
+                                    </ButtonIcon>
+                                </div>
+                                <div>
+                                    <ButtonIcon
+                                    label="Ma liste"
+                                    >
+                                        <font-awesome-icon 
+                                        class="text-3xl" 
+                                        icon="fa-solid fa-play" 
+                                        /> 
+                                    </ButtonIcon>
+                                </div>
+                                <div>
+                                    <ButtonIcon
+                                    label="Telecharger"
+                                    >
+                                        <font-awesome-icon class="text-3xl"
+                                        icon="fa-solid fa-download" 
+                                        /> 
+                                    </ButtonIcon>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="py-8">
+                <p class="text-white text-2xl pl-4 pb-4">Films Similaires</p>
+                <MovieCarousel :movies="similarMovies"></MovieCarousel>
+            </div>
         </div>
-    </div>
-    <div class="py-8">
-        <p class="text-white text-2xl pl-4 pb-4">Films Similaires</p>
-        <MovieCarousel :movies="similarMovies"></MovieCarousel>
-    </div>
+    </TransitionFade>
 </template>
 
 <script setup>
 import { getDetailsMovie,getImage,getSimilarMovies } from '@/data/MovieDb.js'
 import {emptyMovie} from '@/data/movies.js'
 import {ref,onBeforeMount,watch} from 'vue'
-import ButtonIcon from '../components/ButtonIcon.vue';
+import ButtonIcon from '@/components/ButtonIcon.vue';
+import LoadingPage from '@/pages/LoadingPage.vue';
 import MovieCarousel from '@/components/MovieCarousel.vue';
 import { useRoute } from 'vue-router';
+import TransitionFade from '../components/TransitionFade.vue';
+import moment from 'moment'
 
 const movie = ref(emptyMovie)
 const similarMovies = ref()
 const route = useRoute()
+const loading = ref(false)
 
 onBeforeMount(async () => {
-    await getDetailsMovie(route.params.id).then((res) => {
+    await fetchMovieData(route.params.id)
+})
+
+async function fetchMovieData(id){
+    loading.value = true
+    await getDetailsMovie(id).then((res) => {
         console.log(res.data)
         movie.value = res.data
     })
-
-    await getSimilarMovies(route.params.id).then((res) => {
+    await getSimilarMovies(id).then((res) => {
         console.log('similar',res.data.results)
         similarMovies.value = res.data.results
+        
     })
-})
+    setTimeout(() => {loading.value = false},2000)
+}
+
+function formatDate(date){
+    return moment(date).format('DD/MM/YYYY')
+}
 
 watch(() => route.params.id,
 async newId =>{
-    await getDetailsMovie(newId).then((res) => {
-        movie.value = res.data
-    })
-    await getSimilarMovies(newId).then((res) => {
-        similarMovies.value = res.data.results
-    })
+    fetchMovieData(newId)
 })
 </script>
 
