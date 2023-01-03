48a6b6adefda84126aefc77966cca870e2f9f358
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2023-01-03 18:10:04 +0300

    scroll to top behaviour in navigation

--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,11 @@ export const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
     routes, // short for `routes: routes`
-    linkActiveClass: 'text-white underline underline-offset-8'
+    linkActiveClass: 'text-white underline underline-offset-8',
+    scrollBehavior (to, from, savedPosition) {
+        if (to.hash) {
+            return window.scrollTo({ top: document.querySelector(to.hash).offsetTop, behavior: 'smooth' });
+        }
+        return window.scrollTo({ top: 0, behavior: 'smooth' });
+    }   
 })
