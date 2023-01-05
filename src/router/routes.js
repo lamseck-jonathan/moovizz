57e2717d7e350aafa1c62db245adffa413eafda4
jonathan-lam-seck <lamseck.jonathan@gmail.com>
2023-01-05 22:31:59 +0300

    lazy loading pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,19 +4,34 @@ export const routes = [
         component: () => import('@/layout/MainLayout.vue'),
         children:[
             {
+                path:'/',
+                redirect:{name:'homePage'}
+            },
+            {
                 path:'/home',
                 name:'homePage',
-                component: import('@/pages/HomePage.vue')
+                component: () => import('@/pages/HomePage.vue')
             },
             {
                 path:'/detail/:id',
                 name:'detailPage',
-                component: import('@/pages/DetailPage.vue')
+                component: () => import('@/pages/DetailPage.vue')
             },
             {
                 path:'/films',
                 name:'filmsPage',
-                component: import('@/pages/FilmsPage.vue')
+                component: () => import('../pages/FilmsPage.vue')
+            }
+        ]
+    },
+    {
+        path:'/search',
+        component: () => import('@/layout/SecondLayout.vue'),
+        children:[
+            {
+                path:'/search',
+                name:'searchPage',
+                component: () => import('@/pages/SearchPage.vue')
             }
         ]
     }
